8116eda3b3f37ac010e1c02c488992fd793d9a26
Mahir Tekin <mahir_tekin998@hotmail.com>
2024-01-01 15:50:42 +0300

    Login

---
+++ Navigators/StackNavigator.js
@@ -0,0 +1,17 @@
+// StackNavigator.js
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from '../screens/LoginScreen';
+
+
+const Stack = createNativeStackNavigator();
+
+function StackNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={LoginScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default StackNavigator;
